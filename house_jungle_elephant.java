27a12424f4c293d4dd400de0d183ba5fb192ad33
dccleo <136749834+dccleo@users.noreply.github.com>
2023-07-20 17:39:01 +0700

    Fix input sanitization vulnerabilities

---
+++ house_jungle_elephant.java
@@ -0,0 +1,148 @@
+public class Greenovation{
+	//Fields
+	int energySaving = 0;
+
+	//Constructors
+	public Greenovation(){
+		energySaving = 0;
+	}
+
+	public Greenovation(int energySaving){
+		this.energySaving = energySaving;
+	}
+
+	//Methods
+	public int getEnergySaving(){
+		return energySaving;
+	}
+
+	public void setEnergySaving(int energySaving){
+		this.energySaving = energySaving;
+	}
+
+	public void energySavingsReport(){
+		System.out.println("Energy savings report for Greenovation");
+		System.out.println("Total energy savings: "+energySaving+" KWh");
+	}
+
+	public void monitorEnergyUsage(){
+		System.out.println("Monitoring energy usage...");
+	}
+
+	public void solarEnergy(){
+		System.out.println("Solar energy harvesting in progress...");
+	}
+
+	public void windEnergy(){
+		System.out.println("Wind energy harvesting in progress...");
+	}
+
+	public void hydroEnergy(){
+		System.out.println("Hydro energy harvesting in progress...");
+	}
+
+	public void geothermalEnergy(){
+		System.out.println("Geothermal energy harvesting in progress...");
+	}
+
+	public void energyAudit(){
+		System.out.println("Conducting energy audit...");
+	}
+
+	public void energyEfficiency(){
+		System.out.println("Improving energy efficiency...");
+	}
+
+	public void renewableEnergy(){
+		System.out.println("Exploring renewable energy sources...");
+	}
+
+	public void fuelConsumption(){
+		System.out.println("Monitoring fuel consumption...");
+	}
+
+	public void carbonFootprint(){
+		System.out.println("Calculating carbon footprint...");
+	}
+
+	public void wasteManagement(){
+		System.out.println("Implementing waste management plan...");
+	}
+
+	public void waterConservation(){
+		System.out.println("Implementing water conservation plan...");
+	}
+
+	public void treePlanting(){
+		System.out.println("Planting trees...");
+	}
+
+	public void biodiversity(){
+		System.out.println("Preserving biodiversity...");
+	}
+
+	public void resourceConservation(){
+		System.out.println("Conserving resources...");
+	}
+
+	public void greenCommuting(){
+		System.out.println("Encouraging green commuting...");
+	}
+
+	public void products(){
+		System.out.println("Offering environmentally friendly products...");
+	}
+
+	public void ecoLabelling(){
+		System.out.println("Engaging in eco-labelling...");
+	}
+
+	public void recycling(){
+		System.out.println("Promoting recycling...");
+	}
+
+	public void composting(){
+		System.out.println("Developing composting program...");
+	}
+
+	public void urbanGardening(){
+		System.out.println("Starting urban gardening project...");
+	}
+
+	public void energyEfficientAppliances(){
+		System.out.println("Using energy-efficient appliances...");
+	}
+
+	public void energyEfficientLighting(){
+		System.out.println("Installing energy-efficient lighting...");
+	}
+
+	public void smartThermostats(){
+		System.out.println("Using smart thermostats...");
+	}
+
+	public void insulation(){
+		System.out.println("Increasing insulation...");
+	}
+
+	public void greenRoofs(){
+		System.out.println("Installing green roofs...");
+	}
+
+	public void waterEfficientFixtures(){
+		System.out.println("Using water efficient fixtures...");
+	}
+
+	public void lowFlowBathroomFixtures(){
+		System.out.println("Installing low-flow bathroom fixtures...");
+	}
+
+	public void rainGardens(){
+		System.out.println("Creating rain gardens...");
+	}
+
+	public void grayWaterSystems(){
+		System.out.println("Implementing gray water systems...");
+	}
+
+}
